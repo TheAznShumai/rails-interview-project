18ccf64cd30c1decd20f240c458d8304c258a904
Johnny Loi <johnnyloi818@gmail.com>
2016-05-17 15:40:32 -0700

    added tenant, user, qs, and, as counts on the dashboard

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,5 +3,9 @@ class WelcomeController < ApplicationController
   end
 
   def dashboard
+    @api_request_count = Tenant.sum(:api_count)
+    @user_count = User.count
+    @question_count = Question.count
+    @answer_count = Answer.count
   end
 end
