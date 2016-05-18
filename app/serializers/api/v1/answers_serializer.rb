ca38b197e3fe225dfbce678f1f929690614877bb
Johnny Loi <johnnyloi818@gmail.com>
2016-05-18 04:19:15 -0700

    changed root to match answerer naming convention

--- app/serializers/api/v1/answers_serializer.rb
+++ app/serializers/api/v1/answers_serializer.rb
@@ -1,4 +1,4 @@
 class Api::V1::AnswersSerializer < ActiveModel::Serializer
   attributes :id, :question_id, :body
-  has_one :user, root: :provider, serializer: Api::V1::UsersSerializer
+  has_one :user, root: :answerer, serializer: Api::V1::UsersSerializer
 end
